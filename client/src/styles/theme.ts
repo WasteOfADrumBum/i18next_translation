711774c58dddc54feaaadee20829bcef7750cd1a
Joshua Small <JMSmall89@gmail.com>
2024-03-01 21:02:32 -0500

    init styles

---
+++ client/src/styles/theme.ts
@@ -0,0 +1,18 @@
+import { createTheme } from '@mui/material/styles'
+
+// Create a theme instance.
+const theme = createTheme({
+	palette: {
+		primary: {
+			main: '#1976d2',
+		},
+		secondary: {
+			main: '#dc004e',
+		},
+		background: {
+			default: '#fff',
+		},
+	},
+})
+
+export default theme
