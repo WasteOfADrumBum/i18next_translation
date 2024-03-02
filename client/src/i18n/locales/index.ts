178444712db39274926cc5d173440fb7da0fbe32
Joshua Small <JMSmall89@gmail.com>
2024-03-01 20:53:36 -0500

    init i18n

---
+++ client/src/i18n/locales/index.ts
@@ -0,0 +1,17 @@
+import buttons from './components/buttons.json'
+import about from './pages/about.json'
+import contact from './pages/contact.json'
+import home from './pages/home.json'
+import user from './pages/user.json'
+
+const translations = {
+	buttons,
+	pages: {
+		about,
+		contact,
+		home,
+		user,
+	},
+}
+
+export default translations
