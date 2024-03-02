b94951e1b949f398760c6b1906cdbcb775e3b8e8
Joshua Small <JMSmall89@gmail.com>
2024-03-01 20:55:05 -0500

    how to use translations

--- client/src/i18n/locales/index.ts
+++ client/src/i18n/locales/index.ts
@@ -4,6 +4,13 @@ import contact from './pages/contact.json'
 import home from './pages/home.json'
 import user from './pages/user.json'
 
+/*
+ * This file is used to import all the translations from the different files and export them as a single object.
+ * This is useful to import all the translations in a single import statement.
+ * To use the translations in a component, you can import the translations from this file and use them as needed.
+ * For example, to use the translations for the about page, you can import the translations from this file and use them as translations.pages.about.
+ */
+
 const translations = {
 	buttons,
 	pages: {
