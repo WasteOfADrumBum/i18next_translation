cc98cf5230bcafb9863630acb052d2f34448d24a
Joshua Small <jmsmall89@gmail.com>
2024-03-04 16:50:11 -0500

    header context

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, createContext } from 'react'
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import { ThemeProvider } from '@mui/material/styles'
 import { AuthProvider } from './features/auth/AuthProvider'
@@ -21,7 +21,8 @@ import { useDispatch } from 'react-redux'
 import { setUser, clearUser } from './store/actions/userActions'
 
 // Create a Theme Context
-export const ThemeContext = React.createContext<any>(null)
+export const ThemeContext = createContext<any>(null)
+export const HeaderContext = createContext<any>(null)
 
 function PrivateRoute({ element, isAuthenticated }: { element: React.ReactNode; isAuthenticated: boolean }) {
 	return isAuthenticated ? element : <Navigate to='/login' />
@@ -34,6 +35,11 @@ function App() {
 
 	const [darkMode, setDarkMode] = useState<boolean>(true)
 	const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false)
+	const [headerData, setHeaderData] = useState<any>({
+		header: 'React MUI Template',
+		subheader: 'A template for building React applications with Material-UI',
+		extraContent: null,
+	})
 	const dispatch = useDispatch()
 
 	const toggleDarkMode = () => {
@@ -79,71 +85,73 @@ function App() {
 				<ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
 					<CssBaseline />
 					<Router>
-						<NavBar darkMode={darkMode} toggleDarkMode={toggleDarkMode} onLoginToggle={handleLoginToggle} />
-						<div style={{ minHeight: '100vh', marginTop: '64px', marginBottom: '64px' }}>
-							{isAuthenticated && (
-								<Header
-									header='React MUI Template'
-									subHeader='A template for building React applications with Material-UI'
-									user={{ name: 'John Doe', role: 'Admin' }}>
-									<p>Stuff Passed Here</p>
-								</Header>
-							)}
-							<Routes>
-								<Route path='/' element={isAuthenticated ? <Navigate to='/dashboard' /> : <Home />} />
-								<Route path='/login' element={isAuthenticated ? <Navigate to='/dashboard' /> : <Login />} />
-								<Route path='/about' element={<About />} />
-								{/* Private Routes */}
-								<Route
-									path='/dashboard'
-									element={<PrivateRoute element={<Dashboard />} isAuthenticated={isAuthenticated} />}
-								/>
-								<Route
-									path='/dashboard/event/${eventId}/*'
-									element={
-										<PrivateRoute
-											isAuthenticated={isAuthenticated}
-											element={
-												<TabsComponent isAuthenticated={isAuthenticated} tabs={tabs}>
-													<Route path='/details' element={<EventDetailsView />} />
-												</TabsComponent>
-											}
-										/>
-									}
-								/>
-								<Route
-									path='/event/new'
-									element={<PrivateRoute element={<EventInputForm />} isAuthenticated={isAuthenticated} />}
-								/>
-								<Route
-									path='/event/edit/:id'
-									element={<PrivateRoute element={<EventInputForm />} isAuthenticated={isAuthenticated} />}
-								/>
-								<Route
-									path='/event/:id'
-									element={<PrivateRoute element={<EventDetailsView />} isAuthenticated={isAuthenticated} />}
-								/>
-								<Route
-									path='/users'
-									element={<PrivateRoute element={<UserListView />} isAuthenticated={isAuthenticated} />}
-								/>
-								<Route
-									path='/users/new'
-									element={<PrivateRoute element={<UserInputForm />} isAuthenticated={isAuthenticated} />}
-								/>
-								<Route
-									path='/users/edit/:id'
-									element={<PrivateRoute element={<UserInputForm />} isAuthenticated={isAuthenticated} />}
-								/>
-								<Route
-									path='/users/:id'
-									element={<PrivateRoute element={<UserDetailsView />} isAuthenticated={isAuthenticated} />}
-								/>
-								{/* Catch-all route for 404 */}
-								<Route path='*' element={<NotFound />} />
-							</Routes>
-						</div>
-						<Footer />
+						<HeaderContext.Provider value={{ headerData, setHeaderData }}>
+							<NavBar darkMode={darkMode} toggleDarkMode={toggleDarkMode} onLoginToggle={handleLoginToggle} />
+							<div style={{ minHeight: '100vh', marginTop: '64px', marginBottom: '64px' }}>
+								{isAuthenticated && (
+									<Header
+										header={headerData.header}
+										subHeader={headerData.subheader}
+										user={{ name: 'John Doe', role: 'Admin' }}>
+										{headerData.extraContent && <headerData.extraContent />}
+									</Header>
+								)}
+								<Routes>
+									<Route path='/' element={isAuthenticated ? <Navigate to='/dashboard' /> : <Home />} />
+									<Route path='/login' element={isAuthenticated ? <Navigate to='/dashboard' /> : <Login />} />
+									<Route path='/about' element={<About />} />
+									{/* Private Routes */}
+									<Route
+										path='/dashboard'
+										element={<PrivateRoute element={<Dashboard />} isAuthenticated={isAuthenticated} />}
+									/>
+									<Route
+										path='/dashboard/event/${eventId}/*'
+										element={
+											<PrivateRoute
+												isAuthenticated={isAuthenticated}
+												element={
+													<TabsComponent isAuthenticated={isAuthenticated} tabs={tabs}>
+														<Route path='/details' element={<EventDetailsView />} />
+													</TabsComponent>
+												}
+											/>
+										}
+									/>
+									<Route
+										path='/event/new'
+										element={<PrivateRoute element={<EventInputForm />} isAuthenticated={isAuthenticated} />}
+									/>
+									<Route
+										path='/event/edit/:id'
+										element={<PrivateRoute element={<EventInputForm />} isAuthenticated={isAuthenticated} />}
+									/>
+									<Route
+										path='/event/:id'
+										element={<PrivateRoute element={<EventDetailsView />} isAuthenticated={isAuthenticated} />}
+									/>
+									<Route
+										path='/users'
+										element={<PrivateRoute element={<UserListView />} isAuthenticated={isAuthenticated} />}
+									/>
+									<Route
+										path='/users/new'
+										element={<PrivateRoute element={<UserInputForm />} isAuthenticated={isAuthenticated} />}
+									/>
+									<Route
+										path='/users/edit/:id'
+										element={<PrivateRoute element={<UserInputForm />} isAuthenticated={isAuthenticated} />}
+									/>
+									<Route
+										path='/users/:id'
+										element={<PrivateRoute element={<UserDetailsView />} isAuthenticated={isAuthenticated} />}
+									/>
+									{/* Catch-all route for 404 */}
+									<Route path='*' element={<NotFound />} />
+								</Routes>
+							</div>
+							<Footer />
+						</HeaderContext.Provider>
 					</Router>
 				</ThemeProvider>
 			</AuthProvider>
