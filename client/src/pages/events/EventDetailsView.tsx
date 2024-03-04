588a80611fe402a0bbf5bb8f442beb36914c2df8
Joshua Small <jmsmall89@gmail.com>
2024-03-04 17:04:15 -0500

    EventDetailsView setHeaderData

--- client/src/pages/events/EventDetailsView.tsx
+++ client/src/pages/events/EventDetailsView.tsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
+import { HeaderContext } from '../../App'
 
 const EventDetailsView: React.FC = () => {
-	return <div>{/* Your code for Event Details View */}</div>
+	const { setHeaderData } = useContext(HeaderContext)
+
+	useEffect(() => {
+		// Update header data when component mounts
+		setHeaderData({
+			header: 'Event Details',
+			subheader: 'Details for your primary event record',
+			extraContent: () => <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>,
+		})
+
+		// Clean up header data when component unmounts
+		return () => {
+			setHeaderData({
+				header: 'React MUI Template', // Default header
+				subheader: 'A template for building React applications with Material-UI', // Default subheader
+				extraContent: null, // No extra content
+			})
+		}
+	}, [setHeaderData])
+
+	return <div>{/* Code for Event Details View */}</div>
 }
 
 export default EventDetailsView
