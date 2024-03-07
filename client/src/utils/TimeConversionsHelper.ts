9e1c2d447d6cd9bd70bf097d08ae3b82a3979db1
Joshua Small <JMSmall89@gmail.com>
2024-03-06 22:07:37 -0500

    fixed time conversion helper

--- client/src/utils/TimeConversionsHelper.ts
+++ client/src/utils/TimeConversionsHelper.ts
@@ -11,45 +11,18 @@ const TimeConversionsHelper = {
 		}
 
 		let formattedDate = ''
-
-		if (format.includes('MM')) {
-			const month = date.getMonth() + 1
-			formattedDate += month.toString().padStart(2, '0') + '/'
+		const options: Intl.DateTimeFormatOptions = {
+			timeZone: timeZone,
+			year: 'numeric',
+			month: format.includes('MM') ? '2-digit' : undefined,
+			day: format.includes('DD') ? '2-digit' : undefined,
+			hour: format.includes('hh') || format.includes('HH') ? '2-digit' : undefined,
+			minute: format.includes('mm') ? '2-digit' : undefined,
+			second: format.includes('ss') ? '2-digit' : undefined,
+			hour12: format.includes('hh'),
 		}
 
-		if (format.includes('DD')) {
-			const day = date.getDate()
-			formattedDate += day.toString().padStart(2, '0') + '/'
-		}
-
-		if (format.includes('YYYY')) {
-			const year = date.getFullYear()
-			formattedDate += year
-		}
-
-		if (includeTime) {
-			formattedDate += ' '
-
-			if (format.includes('hh')) {
-				const hours12 = date.getHours() % 12 || 12 // Convert 24-hour to 12-hour format
-				formattedDate += hours12.toString().padStart(2, '0') + ':'
-			}
-
-			if (format.includes('HH')) {
-				const hours24 = date.getHours()
-				formattedDate += hours24.toString().padStart(2, '0') + ':'
-			}
-
-			if (format.includes('mm')) {
-				const minutes = date.getMinutes()
-				formattedDate += minutes.toString().padStart(2, '0')
-			}
-
-			if (format.includes('ss')) {
-				const seconds = date.getSeconds()
-				formattedDate += ':' + seconds.toString().padStart(2, '0')
-			}
-		}
+		formattedDate = date.toLocaleString(undefined, options)
 
 		return formattedDate.trim()
 	},
