12fb93fdc0d2a697aeceaea60d85fc31306a2278
Joshua Small <JMSmall89@gmail.com>
2024-03-09 13:02:39 -0500

    Log pg connection status

--- server/postgresConfig.js
+++ server/postgresConfig.js
@@ -9,5 +9,15 @@ const pool = new Pool({
 	port: 5432,
 })
 
+// Log connection status
+pool.connect((err, client, release) => {
+	if (err) {
+		console.error('\x1b[31mError connecting to PostgreSQL database:', err)
+	} else {
+		console.log('\x1b[32mConnected to PostgreSQL database')
+		release()
+	}
+})
+
 // Export the pool object
 module.exports = pool
