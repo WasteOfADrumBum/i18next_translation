01ec1827a1c54f312433230f72bceb5f0399619d
Joshua Small <jmsmall89@gmail.com>
2024-03-15 17:29:24 -0400

    MongoDB: Action Read Event failing

--- client/src/pages/events/EventDetailsView.tsx
+++ client/src/pages/events/EventDetailsView.tsx
@@ -1,8 +1,28 @@
 import React, { FC, useContext, useEffect } from 'react'
 import { HeaderContext } from '../../contexts/HeaderContext'
+import { Dispatch } from 'redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { useParams } from 'react-router-dom'
+import { readEvent } from '../../store/actions/mongodb/eventActions'
+import { RootState } from 'store'
+import { Container, Typography } from '@mui/material'
 
 const EventDetailsView: FC = () => {
 	const { setHeaderData } = useContext(HeaderContext)
+	const dispatch: Dispatch<any> = useDispatch()
+	const { eventId } = useParams()
+
+	useEffect(() => {
+		console.log(eventId)
+		// Fetch event details from Redux store
+		if (eventId) {
+			// Check if eventId is not undefined
+			dispatch(readEvent(eventId))
+		}
+	}, [dispatch, eventId])
+
+	// Access event details from Redux store
+	const { event, loading, error } = useSelector((state: RootState) => state.events)
 
 	useEffect(() => {
 		// Update header data when component mounts
@@ -22,7 +42,22 @@ const EventDetailsView: FC = () => {
 		}
 	}, [setHeaderData])
 
-	return <div>{/* Code for Event Details View */}</div>
+	useEffect(() => {
+		console.log(event)
+	}, [event])
+
+	return (
+		<Container maxWidth='xl'>
+			{/* Display event details */}
+			{loading ? (
+				<Typography variant='h6'>Loading...</Typography>
+			) : typeof error === 'object' && Object.keys(error).length !== 0 ? (
+				<Typography variant='h6'>Error: {error.toString()}</Typography>
+			) : (
+				<>Event</>
+			)}
+		</Container>
+	)
 }
 
 export default EventDetailsView
