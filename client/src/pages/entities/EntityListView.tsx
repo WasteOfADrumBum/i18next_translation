9cd7baa982fef5381e9ad02f730886bd82a104f9
Joshua Small <JMSmall89@gmail.com>
2024-03-16 21:45:48 -0400

    added headers to list vies

--- client/src/pages/entities/EntityListView.tsx
+++ client/src/pages/entities/EntityListView.tsx
@@ -1,6 +1,29 @@
-import React, { FC } from 'react'
+import { Grid } from '@mui/material'
+import { HeaderContext } from '../../contexts/HeaderContext'
+import React, { FC, useContext, useEffect } from 'react'
 
 const EntityListView: FC = () => {
+	const { setHeaderData } = useContext(HeaderContext)
+
+	useEffect(() => {
+		// Update header data when component mounts
+		setHeaderData({
+			header: 'Entities',
+			subheader: 'Entities associated with your primary event record',
+			extraContent: <Grid container spacing={1}></Grid>,
+			returnButton: true,
+		})
+
+		// Clean up header data when component unmounts
+		return () => {
+			setHeaderData({
+				header: 'React MUI Template', // Default header
+				subheader: 'A template for building React applications with Material-UI', // Default subheader
+				extraContent: null, // No extra content
+			})
+		}
+	}, [setHeaderData])
+
 	return <>Entity List</>
 }
 
