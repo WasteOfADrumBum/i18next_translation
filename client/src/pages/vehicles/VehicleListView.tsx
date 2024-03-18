30738ab28fe5c0b614db17ccb82f0f2439406465
Joshua Small <JMSmall89@gmail.com>
2024-03-17 21:15:18 -0400

    init vehilce list view

--- client/src/pages/vehicles/VehicleListView.tsx
+++ client/src/pages/vehicles/VehicleListView.tsx
@@ -1,16 +1,41 @@
-import { Grid } from '@mui/material'
+import React, { useContext, useEffect, FC } from 'react'
+import { Grid, Button, Container, Typography, capitalize } from '@mui/material'
+import { AddCircleOutline } from '@mui/icons-material'
+import { useDispatch, useSelector } from 'react-redux'
+import { Dispatch } from 'redux'
+import { useNavigate } from 'react-router-dom'
 import { HeaderContext } from '../../contexts/HeaderContext'
-import React, { FC, useContext, useEffect } from 'react'
+import { getVehicles } from '../../store/actions/mongodb/vehicleActions'
+import { RootState } from '../../store'
+import { Vehicle } from '../../store/types/VehicleTypes'
+import { DynamicDataTable, ActionsMenu } from '../../components'
+import { ExtractLastFiveDigits } from '../../utils'
 
 const VehicleListView: FC = () => {
 	const { setHeaderData } = useContext(HeaderContext)
+	const dispatch: Dispatch<any> = useDispatch()
+	const navigate = useNavigate()
+
+	// Fetch vehicles from Redux store
+	useEffect(() => {
+		dispatch(getVehicles())
+	}, [dispatch])
+
+	// Access vehicles from Redux store
+	const { vehicles, loading, error } = useSelector((state: RootState) => state.vehicles)
 
 	useEffect(() => {
 		// Update header data when component mounts
 		setHeaderData({
 			header: 'Vehicles',
 			subheader: 'Vehicles associated with your primary event record',
-			extraContent: <Grid container spacing={1}></Grid>,
+			extraContent: (
+				<Grid container spacing={1}>
+					<Grid item>
+						<Typography>Total Vehicles: {vehicles.length}</Typography>
+					</Grid>
+				</Grid>
+			),
 			returnButton: true,
 		})
 
@@ -22,9 +47,67 @@ const VehicleListView: FC = () => {
 				extraContent: null, // No extra content
 			})
 		}
-	}, [setHeaderData])
+	}, [setHeaderData, vehicles.length])
 
-	return <>Vehicle List</>
+	const handleView = (id: string | undefined) => {
+		if (id) {
+			navigate(`/dashboard/vehicle/${id}/details`)
+		}
+	}
+
+	const handleEdit = (id: string | undefined) => {
+		if (id) {
+			navigate(`/dashboard/vehicle/${id}/edit`)
+		}
+	}
+
+	const handleDelete = (id: string | undefined) => {
+		// Implement delete logic as needed
+	}
+
+	const columns = [
+		{
+			id: '_id',
+			label: 'ID',
+			render: (data: Vehicle) => <Typography>{data._id ? ExtractLastFiveDigits(data._id) : 'N/A'}</Typography>,
+		},
+		{
+			id: 'actions',
+			label: 'Actions',
+			render: (data: Vehicle) => (
+				<ActionsMenu
+					onView={() => handleView(data._id ?? '')}
+					onEdit={() => handleEdit(data._id ?? '')}
+					onDelete={() => handleDelete(data._id ?? '')}
+				/>
+			),
+		},
+	]
+
+	return (
+		<Container maxWidth='xl'>
+			<Grid container justifyContent='flex-end'>
+				<Button onClick={() => navigate('/vehicle/create')} sx={{ margin: 1 }}>
+					<AddCircleOutline sx={{ marginRight: 1 }} /> Add Vehicle
+				</Button>
+			</Grid>
+			{loading ? (
+				<Typography variant='h6'>Loading...</Typography>
+			) : typeof error === 'object' && Object.keys(error).length !== 0 ? (
+				<Typography variant='h6'>Error: {error.toString()}</Typography>
+			) : (
+				<DynamicDataTable
+					data={vehicles}
+					columns={columns}
+					rowsPerPageOptions={[5, 10, 25]}
+					pagination={{ rowsPerPage: 5 }}
+					page={0}
+					onPageChange={(newPage) => console.log('Page changed to:', newPage)}
+					onRowsPerPageChange={(newRowsPerPage) => console.log('Rows per page changed to:', newRowsPerPage)}
+				/>
+			)}
+		</Container>
+	)
 }
 
 export default VehicleListView
--- client/src/store/reducers/index.ts
+++ client/src/store/reducers/index.ts
@@ -1,10 +1,14 @@
 import { combineReducers } from 'redux'
 import userReducer from './mongodb/userReducer'
 import eventReducer from './mongodb/eventReducer'
+import entityReducer from './mongodb/entityReducer'
+import vehicleReducer from './mongodb/vehicleReducer'
 
 const rootReducer = combineReducers({
 	user: userReducer,
 	events: eventReducer,
+	entities: entityReducer,
+	vehicles: vehicleReducer,
 })
 
 export default rootReducer
