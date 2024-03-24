a0639245056d9711301aab762de3deb31444fe33
Joshua Small <JMSmall89@gmail.com>
2024-03-23 20:44:17 -0400

    spanish translations

--- client/src/i18n/i18n.ts
+++ client/src/i18n/i18n.ts
@@ -2,16 +2,16 @@ import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 
 // Import translations
-import { enTranslation } from './locales'
+import { enTranslation, esTranslation } from './locales'
 
 i18n.use(initReactI18next).init({
 	resources: {
 		en: {
 			translation: enTranslation,
 		},
-		// es: {
-		// 	translation: esTranslation,
-		// },
+		es: {
+			translation: esTranslation,
+		},
 	},
 	fallbackLng: 'en', // Fallback language
 	debug: true, // Enable debug mode
