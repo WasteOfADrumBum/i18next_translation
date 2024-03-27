4117465d0278797d7f0be7ce6006aead7aa22844
Joshua Small <jmsmall89@gmail.com>
2024-03-27 09:37:38 -0400

    init chartjs line chart

---
+++ client/src/components/charts/LineChart.tsx
@@ -0,0 +1,65 @@
+import {
+	CategoryScale,
+	Chart as ChartJS,
+	Legend,
+	LinearScale,
+	LineElement,
+	PointElement,
+	Title,
+	Tooltip,
+} from 'chart.js'
+import React, { FC } from 'react'
+import { Line } from 'react-chartjs-2'
+
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend)
+
+interface LineChartProps {
+	labels: string[]
+	datasets: {
+		label: string
+		data: number[]
+		borderColor: string
+		backgroundColor: string
+	}[]
+	title?: string
+}
+
+interface LineChartOptions {
+	responsive?: boolean
+	plugins?: {
+		legend?: {
+			position?: 'top' | 'right' | 'bottom' | 'left'
+		}
+		title?: {
+			display?: boolean
+			text?: string
+		}
+	}
+}
+
+const LineChart: FC<LineChartProps> = ({ labels, datasets, title }) => {
+	const displayTitle = !!title
+	const titleText = title || ''
+
+	const options: LineChartOptions = {
+		responsive: true,
+		plugins: {
+			legend: {
+				position: 'top',
+			},
+			title: {
+				display: displayTitle,
+				text: titleText,
+			},
+		},
+	}
+
+	const data = {
+		labels: labels,
+		datasets: datasets,
+	}
+
+	return <Line options={options} data={data} />
+}
+
+export default LineChart
