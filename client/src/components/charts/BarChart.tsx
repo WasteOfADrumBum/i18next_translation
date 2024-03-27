59f77a9e789d27bd677d60b9acfe3debd5be8f46
Joshua Small <jmsmall89@gmail.com>
2024-03-27 09:26:07 -0400

    init chartjs bar chart

---
+++ client/src/components/charts/BarChart.tsx
@@ -0,0 +1,54 @@
+import { BarController, CategoryScale, Chart as ChartJS, Legend, LinearScale, Title, Tooltip } from 'chart.js'
+import React, { FC } from 'react'
+import { Bar } from 'react-chartjs-2'
+
+ChartJS.register(BarController, CategoryScale, LinearScale, Title, Tooltip, Legend)
+
+interface BarChartProps {
+	labels: string[]
+	datasets: {
+		label: string
+		data: number[]
+		borderColor: string
+		backgroundColor: string
+	}[]
+	vertical?: boolean
+	title?: string
+}
+
+interface BarChartOptions {
+	indexAxis?: 'x' | 'y'
+	responsive?: boolean
+	plugins?: {
+		title?: {
+			display?: boolean
+			text?: string
+		}
+	}
+}
+
+const BarChart: FC<BarChartProps> = ({ labels, datasets, vertical, title }) => {
+	const indexAxis = vertical ? 'y' : 'x'
+	const displayTitle = !!title
+	const titleText = title || ''
+
+	const options: BarChartOptions = {
+		indexAxis: indexAxis,
+		responsive: true,
+		plugins: {
+			title: {
+				display: displayTitle,
+				text: titleText,
+			},
+		},
+	}
+
+	const data = {
+		labels: labels,
+		datasets: datasets,
+	}
+
+	return <Bar options={options} data={data} />
+}
+
+export default BarChart
